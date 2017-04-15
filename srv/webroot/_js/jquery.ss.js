284fd82c3ad664352414c524f25dd792553dc9e5
puppetmaster3 <vicmasons@gmail.com>
2017-04-14 21:39:13 -0400

    f

--- srv/webroot/_js/jquery.ss.js
+++ srv/webroot/_js/jquery.ss.js
@@ -1,7 +1,31 @@
 /*!
- jquery.ss.js progressively loads pages for a single-page application. 
- Based on SmoothState.js 
+ jquery.ss.js loads pages, a single-page application implementation. 
+ Based on SmoothState.js.
+ It requires jquery and js-Singals.
 */
 
-
 console.log('ss')
+$(document).ready(function () {
+	$('a').click(function (e) {
+		console.log(e)
+		console.log(e.target.tagName)
+
+		console.log(e.target.baseURI)
+		console.log(e.target.href)
+
+		e.preventDefault();
+		e.stopPropagation();
+	})
+})
+
+class SS {
+load(pg) {
+	console.log(pg)
+}//()
+clear(id) {
+	console.log(id)
+}
+}//class
+var ss = new SS()
+
+
