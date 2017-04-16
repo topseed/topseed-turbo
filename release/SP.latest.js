8773f72be7301934fa9a3524e42b9eb0574682af
puppetmaster3 <vicmasons@gmail.com>
2017-04-16 16:59:56 -0400

    d

--- release/SP.latest.js
+++ release/SP.latest.js
@@ -1,7 +1,7 @@
 // https://rawgit.com/
 
 /*!
- jquery.SP.js loads pages, a single-page application implementation. 
+ SmoothPage loads pages, a single-page application implementation. 
  Based on SmoothState.js. It uses jquery slim, fetch, loosely coupled and does not load the app shell DOM, only content.
  It requires jquery and js-Signals. In ie it requiers fetch
 */
