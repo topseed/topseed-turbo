a5eb5de62d2e54f458f03b876c2c843a108e7c3c
puppetmaster3 <vicmasons@gmail.com>
2017-04-17 15:41:06 -0400

    dd

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SmoothPage
 
  SmoothPage loads pages, a single-page application implementation. 
- Based on SmoothState.js. It uses jquery slim, fetch, loosely coupled and does not load the app shell DOM, only content.
- It requires jquery and js-Signals. In ie it requiers fetch.
+ It uses jquery slim, fetch, loosely coupled and does not load the app shell DOM, only content.
+ It requires jquery and js-Signals. In ie it requires fetch.
  When using w/ side bar, you need to handle it.
 
  This is a fork of SmoothState, SmoothState may be able to handle more corner cases.
--- release/SP.latest.js
+++ release/SP.latest.js
@@ -1,12 +1,5 @@
 // https://rawgit.com/
 
-/*!
- SmoothPage loads pages, a single-page application implementation. 
- Based on SmoothState.js. It uses jquery slim, fetch, loosely coupled and does not load the app shell DOM, only content.
- It requires jquery and js-Signals. In ie it requiers fetch.
- When using w/ side bar, you need to handle it
-*/
-
 /*ex pg use:
 	function init() {
 	}
@@ -127,7 +120,7 @@ ScontentID: '#myContentId' //the content in your layout. The rest should be app 
 		,title : title
 	}
 
-	history.pushState( SP._lastState, title, SP.stripHash(href))//title will not be used
+	history.pushState( SP._lastState, title, SP.stripHash(href))//title will not be used, it is loaded in loadPg()
 	SP.loadPg(href)
 }//()
 
