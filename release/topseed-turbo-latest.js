0cdc4fa7e14b0c47501c096e66ea4d2a63758ab2
puppetmaster3 <vicmasons@gmail.com>
2017-05-10 07:42:59 -0400

    Added setup so turbo wakes up

---
+++ release/topseed-turbo-latest.js
@@ -0,0 +1,133 @@
+// http://zinoui.com/demo/pushstate
+
+'use strict'
+
+//setup page events /////////////////////////
+$(document).ready(function () {
+
+	TT.clearUrl()
+
+	$(window).on('popstate', function (e) {//back button
+		var state = e.originalEvent.state
+		if (state !== null) {
+			e.preventDefault()
+			TT.loadPg(state.url)
+		}
+	})//()
+
+	$(document).on('click', 'a', function (e) {//prevent
+		var $anchor = $(e.currentTarget)
+		var href = $anchor.prop('href')
+		//console.log(href)
+		if(! href || href.length < 1) {
+			return
+		}
+		if(TT.isHash(href)) {
+			TT.clearUrl()
+			console.log('#')
+			return
+		}
+		if(TT.isExternal(href)) {
+			console.log('bye')
+			return
+		}
+
+		//e.stopPropagation()
+		e.preventDefault()
+		TT._clickAnchor(href)
+	})//()
+	console.log('TT ready')
+})
+
+///////////////////////////////////////////////////////
+var TT = { //class:
+	
+ScontentID: '#myContentId' //the content in your layout. The rest should be app shell from PWA.
+,_setupStarted: new Date().getTime()
+,smoothPg: new signals.Signal()	
+,inAction : false // set to true when user acts; false when effect is done
+,PRE : '_pre-action'
+,PAGE : '_new-page'
+,_actStarted : new Date().getTime()
+,startAct: function (newUrl) {
+	TT.inAction = true
+	TT._actStarted = new Date().getTime()
+	TT.smoothPg.dispatch(TT.PRE, newUrl)
+}//()
+,actReady: function ($newContent, $html) {
+	var delta = new Date().getTime() - TT._actStarted
+	TT.smoothPg.dispatch(TT.PAGE, $newContent, delta, $html)
+	TT.inAction=false
+
+}//()
+
+,loadPg: function(pg) {//triggered, but funtion can be called directly also
+	TT.startAct(TT.stripHash(pg))//maybe just #sidedrawer
+	fetch(pg, {
+			method: 'get'
+		}).then(function(reSPonse) {
+			if (!reSPonse.ok) {
+				console.log('not ok')
+				console.log(reSPonse)
+				throw Error(reSPonse.statusText)
+			}
+			return reSPonse.text()
+		}).then(function(txt) {
+			var $html = $( '<html></html>' ).append( $(txt) )
+			var title = $html.find('title').first().text()
+			document.title = title
+
+			var div = $html.find(TT.ScontentID).html()
+
+			TT.actReady(div, $html)
+
+		}).catch(function(err) {
+			console.log(err)
+			TT.smoothPg.dispatch('ERROR',err)
+	})//fetch
+
+}//()
+
+,_lastState: {} // maybe used 
+,_clickAnchor : function(href) {
+	TT._lastState =  {
+		url : href
+	}
+
+	history.pushState( TT._lastState, '', TT.stripHash(href))//title will not be used, it is loaded in loadPg()
+	TT.loadPg(href)
+}//()
+
+,isExternal: function(url) {// copied from original SS
+	var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/)
+	if (typeof match[1] === 'string' && match[1].length > 0 && match[1].toLowerCase() !== window.location.protocol) {
+		return true
+	}
+	if (typeof match[2] === 'string' &&
+		match[2].length > 0 &&
+		match[2].replace(new RegExp(':(' + {'http:': 80, 'https:': 443}[window.location.protocol] +
+		')?$'), '') !== window.location.host) {
+		return true
+	}
+	return false
+}//()
+,stripHash: function(href) {
+	if(undefined === href) return undefined
+	return href.replace(/#.*/, '')
+}
+,isHash: function (href) {//maybe only #sidedrawer
+	return href.indexOf('#') > -1
+}
+
+,clearUrl:function () {// ?
+	var url = location.pathname
+	var h = TT.stripHash(url) //maybe only #sidedrawer
+	console.log(h)
+	window.location.hash = ''
+	history.replaceState(TT._lastState, document.title, h)
+}
+}//class
+
+window.addEventListener('pageshow', function(event) {
+	console.log('pageshow:', event.timeStamp)
+})
---
+++ setup/setup-2.0.js
@@ -0,0 +1,84 @@
+
+// in 'main.js': signalAppReady() // app ready sent 
+/*ex pg use:
+	onAppReady(UIinit)
+
+	function UIinit() {
+	}
+*/
+'use strict'
+
+function loadNX(lib, xfoo) { //load and exec
+	loadjs([ lib ], // now load ps
+		{ success: function(){ 
+			xfoo()
+		} 
+		})//load ps.js	
+}
+
+// load stuff:
+console.log('ie check:')
+loadjs([ // load bowser
+	'https://cdn.rawgit.com/topseed/topseed-turbo/master/deps/bowser.min.js'
+	], { success: function(){
+			if(bowser.msie) {
+				console.log('-you got IE, not edge')
+				loadIE()
+			} else {
+				loadjs.done('dependencyIE')
+			}
+	}, async: false
+})
+function loadIE() { //load fetch and reactive image poly, since not in IE
+	loadjs([
+		 '//cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js'
+		,'//cdn.jsdelivr.net/picturefill/3.0.3/picturefill.min.js'
+		], { success: function(){
+			console.log('loaded dependencyIE')
+			loadjs.done('dependencyIE')
+		}
+	})
+}
+
+window.appSetup = null// signal
+window.appReady = false
+loadjs([
+	'//cdn.jsdelivr.net/js-signals/1.0.0/signals.min.js'
+	,'https://cdn.rawgit.com/topseed/topseed-turbo/master/deps/js.cookie.min.js'
+
+	,'https://rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-2.0.js'
+
+	], { success: function(){
+		console.log('setup libs loaded')
+		$(document).ready(function () {// doc ready and libs loaded
+			//app ready
+			window.appSetup = new signals.Signal()	
+
+			loadjs.done('keyLibs')
+		})
+	}, async: false
+})
+function signalAppReady() {
+	window.appReady = true
+	window.appSetup.dispatch('app-ready')
+}
+function onAppReady(pinit) {
+	if(appReady) {
+		console.log('app-ready!')
+		pinit()
+	} else if(appSetup) {
+		appSetup.addOnce(function(e){
+			console.log('app-ready!!')
+			pinit() 
+		})//app 
+	} else {
+		setTimeout(function() {//wait X milliseconds then recheck
+			console.log('.')
+			onAppReady(pinit)
+		} ,50)
+	}//else
+}//()
+
+window.onbeforeunload = function (e) {
+	console.log('please come back soon')
+}
