3451be62bae7e37943044cbbd5a1a837d765d8ca
Wolfgang <wgehner@gmail.com>
2017-07-25 14:09:29 -0700

    Add async importHref

---
+++ webComps/tp1-1.2.js
@@ -0,0 +1,37 @@
+//Loaded from main.js
+//Support for Polymer Components
+var TP = { //class:
+	_loadedComp : {'exComp': true} // don't load 2x
+
+	, loadComp : function(url){
+		return new Promise(function (resolve, reject){
+			if (url in TP._loadedComp) {//guard: we loaded it before, thank you very much
+				console.log('already loaded')
+				resolve("OK2")
+			} else {
+				fetch(url, {
+					method: 'get'
+				}).then(function(response) {
+					if (!response.ok) {
+						console.log('not ok')
+						console.log(response)
+						reject(response.statusText)
+					} 
+					return response.text()
+				}).then(function(txt) {
+					//registerComp = function(){} //null function	
+					TP._loadedComp[url] = true
+					console.log('loading (again?):', url)
+
+					//firefox needs whenReady
+					HTMLImports.whenReady(function() {
+						Polymer.Base.importHref(url, function() {
+							console.log('importHref done')
+							return resolve('OK')
+						}, function(){return reject('Not OK')}, true)
+					})
+				})	
+			}
+		})	
+	}
+}
